7fe442a37c35054e2b425d05129851d106136071
hildeha <56960292+hildeha@users.noreply.github.com>
2021-08-14 21:38:01 +0200

    Update peterandrbae_short.py

--- peterandrbae_short.py
+++ peterandrbae_short.py
@@ -1,1 +1,65 @@
+from Faces import Detection
+from Utils import help
+import streamlit as st
+from PIL import Image
+import numpy as np
+import base64
 
+
+def main():
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+
+    st.markdown('<h2 style="text-align:center;"> <span style="color:#F633FF"> Make sure you have enough Peter André '
+                'in your life <br><br>Upload a pic and see if Pete is in it!'
+                '</span> 👏 </h2> ', unsafe_allow_html=True)
+
+
+
+    #picture = st.file_uploader(label='Picture goes here')
+
+    picture = st.file_uploader(label='Picture goes here', type=['png', 'jpg', 'jpeg'])
+    if picture is not None:
+
+
+        #image = Image.read(picture)
+        img = Image.open(picture)
+        image = np.asarray(img)
+
+
+        # DETECT FACES
+
+        detector = Detection.get_detector()
+        img_faces = Detection.extract_face_from_image(image, detector)
+
+        petes = Detection.get_petes(detector)
+        boolean = Detection.find_pete(petes, image, detector)
+
+        fig = Detection.highlight_faces_bool(image, detector, boolean)
+
+        if len(img_faces) > 1:
+            st.markdown('<h2 style="text-align:center;"> There are <span style="color:#F633FF"> {} </span> '
+                        'faces in '
+                        'your picture! </h2> '.format(len(img_faces)), unsafe_allow_html=True)
+
+        if len(img_faces) == 1:
+            st.markdown('<h2 style="text-align:center;"> There is <span style="color:#F633FF"> {} </span> '
+                        'face in '
+                        'your picture! </h2> '.format(len(img_faces)), unsafe_allow_html=True)
+
+
+        # extracted_faces = extract_face_from_image(image, faces)
+        if len([x for x in boolean if x is True]) > 0:
+            st.markdown('<h2 style="text-align:center;"> AND <span style="color:#FFCD33"> {} </span> '
+                        'IS OF PETE!!!</h2> '.format(len([x for x in boolean if x is True])), unsafe_allow_html=True)
+        else:
+            st.markdown('<h2 style="text-align:center;"> ..and  <span style="color:blue"> none </span> '
+                        'is of Pete. Boo. That picture sucks.<h2> ', unsafe_allow_html=True)
+
+        st.pyplot(fig)
+
+        if len([x for x in boolean if x is True]) > 0:
+            st.balloons()
+
+
+if __name__ == "__main__":
+    main()
